be1c8ba9cad6596a47138f952cc2dd3791782806
Lucas Oliveira <lucas.oliveira@layers.education>
2023-08-21 23:20:35 -0300

    fix: change objective service to helper

--- src/stores/objective.ts
+++ src/stores/objective.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import * as ObjectiveService from "@/services/objective-service";
+import * as ObjectiveService from "@/helpers/objective";
 
 interface ObjectiveStore {
   loading: boolean;
