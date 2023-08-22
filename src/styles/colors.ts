48f2032f6a5af3d606c88268a99f44edda69e3cf
Lucas Oliveira <lucas.oliveira@layers.education>
2023-08-22 00:44:02 -0300

    feat: define base app colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const AppColors = {
+  black: "#333333",
+  gray: "#777777",
+  lightGray: "#EEEEEE",
+  blue: "#1B9CFA",
+  lightBlue: "#74C5FF",
+  white: "#FFFFFF",
+  green: "#32D32F",
+  purple: "#7E46DA",
+  red: "#DA4646",
+  lightRed: "#F39292",
+  yellow: "#FFC940",
+  lightYellow: "#FFF3B5",
+};
