ebb87133594f180e3fdd98fbc6650a7d3a68544e
Lucas Oliveira <lucas.oliveira@layers.education>
2023-08-24 01:17:55 -0300

    feat: add generic version type

--- src/interfaces/version.ts
+++ src/interfaces/version.ts
@@ -1,4 +1,5 @@
 declare namespace Bible {
   export type EnglishVersion = "A" | "B" | "C";
   export type PortugueseVersion = "ARA" | "ARC" | "NVT";
+  export type Version = EnglishVersion | PortugueseVersion;
 }
