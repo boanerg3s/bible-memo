c99fccda74c5c9639b51fd569b3bc861294d74e8
Lucas Oliveira <lucas.oliveira@layers.education>
2023-08-24 00:28:28 -0300

    chore: add localization related libs

--- metro.config.ts
+++ metro.config.ts
@@ -1,4 +1,13 @@
 import { getDefaultConfig } from "expo/metro-config";
 
 const config = getDefaultConfig(__dirname);
-module.exports = config;
+const defaultSourceExts = config.resolver?.sourceExts || [];
+const extendedSourceExts = ["jsx", "js", "ts", "tsx", "json", "svg", "d.ts", "mjs"];
+
+module.exports = {
+  ...config,
+  resolver: {
+    ...config.resolver,
+    sourceExts: [...defaultSourceExts, ...extendedSourceExts],
+  },
+};
