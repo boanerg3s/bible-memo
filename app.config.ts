9cab703498d76b4db4f6c66ee4a3c176e0373bfa
Lucas Oliveira <lucas.oliveira@layers.education>
2023-08-31 17:19:23 -0300

    fix: app versions

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   name: "bible-memo-app",
   slug: "bible-memo-app",
   scheme: "bible-memo-app",
-  version: "1.0.0",
+  version: "0.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
